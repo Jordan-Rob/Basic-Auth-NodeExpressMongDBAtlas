1c4b77cdaae2662805a45c0684d53b7303c7e0b5
jordanrob709 <jordanrob709@gmail.com>
2020-07-06 16:52:02 +0300

    added login route authentication and  validation

--- api/api.js
+++ api/api.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const user = require('../models/users')
-const { signupChecks } = require('../authValidation')
+const { signupChecks, loginChecks } = require('../authValidation')
 const bcrypt = require('bcrypt')
+const { request } = require('express')
 
 router.post('/signup', async (request, response) => {
     const { error } = signupChecks(request.body)
@@ -30,6 +31,24 @@ router.post('/signup', async (request, response) => {
         .catch((error) => {
             response.json(error)
         })
+})
+
+router.post('/login', async (request, response) => {
+    const { error } = loginChecks(request.body)
+    if (error) {
+        return response.status(400).send(error.details[0].message)
+    }
+
+    const appUser = await user.findOne({ email: request.body.email })
+    if (!appUser) {
+        return response.status(400).send('cannot find user with entered email')
+    }
+
+    const correctPassword = await bcrypt.compare(request.body.password, appUser.password)
+    if (!correctPassword) {
+        return response.status(400).send('Incorrect password entered!')
+    }
+    return response.send('logged in successfully')
 })
 
 module.exports = router
--- authValidation.js
+++ authValidation.js
@@ -9,4 +9,14 @@ const signupChecks = (data) => {
     return validationSchema.validate(data)
 }
 
+const loginChecks = (data) => {
+    const validationSchema = joi.object({
+        email: joi.string().min(5).email().required(),
+        password: joi.string().min(4).required()
+    })
+    return validationSchema.validate(data)
+}
+
+
 module.exports.signupChecks = signupChecks
+module.exports.loginChecks = loginChecks
